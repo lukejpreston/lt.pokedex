58f696fb2fd1f1058492f08d2ebc2677db3406a3
Luke Preston <luke.preston@bbc.co.uk>
2017-05-18 07:41:06 +0100

    forms

--- src/db/pokemon/forms.js
+++ src/db/pokemon/forms.js
@@ -1,5 +1,12 @@
 module.exports = (db, p) => {
   const pokemonFormsCollection = db.getCollection('_pokemon_forms')
   if (p.id === 12) console.log(pokemonFormsCollection.data[0])
-  return []
+  return pokemonFormsCollection
+    .find({pokemon_id: p.id})
+    .map(pf => {
+      return {
+        url: `http://pokeapi.co/api/v2/pokemon-form/${pf.id}/`,
+        name: pf.identifier
+      }
+    })
 }
