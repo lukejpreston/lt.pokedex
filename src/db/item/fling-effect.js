ab23c2a627163f995e5fca7b92dae643ed7771fd
Luke Preston <luke.preston@bbc.co.uk>
2017-05-20 15:31:33 +0100

    item fling effect

--- src/db/item/fling-effect.js
+++ src/db/item/fling-effect.js
@@ -1,3 +1,11 @@
 module.exports = (db, i) => {
-  return null
+  if (i.fling_effect_id === '') return null
+
+  const itemFlingEffectsCollection = db.getCollection('_item_fling_effects')
+  const itemFlingEffect = itemFlingEffectsCollection.findOne({id: i.fling_effect_id})
+
+  return {
+    url: `http://pokeapi.co/api/v2/item-fling-effect/${itemFlingEffect.id}/`,
+    name: itemFlingEffect.name
+  }
 }
