d8ff721af6e443c707eecec99ec65be4234f95fa
Luke Preston <luke.preston@bbc.co.uk>
2017-05-22 12:30:27 +0100

    move meta

--- src/db/move/meta.js
+++ src/db/move/meta.js
@@ -1,8 +1,14 @@
 module.exports = (db, m) => {
   const moveMetaCollection = db.getCollection('_move_meta')
+  const moveMetaAilmentsCollection = db.getCollection('_move_meta_ailments')
+  const moveMetaCategoriesCollection = db.getCollection('_move_meta_categories')
+
   const moveMeta = moveMetaCollection.findOne({move_id: m.id})
 
   if (moveMeta === null) return null
+
+  const moveMetaAilment = moveMetaAilmentsCollection.findOne({id: moveMeta.meta_ailment_id})
+  const moveMetaCategory = moveMetaCategoriesCollection.findOne({id: moveMeta.meta_category_id})
 
   const minTurns = moveMeta.min_turns === '' ? null : moveMeta.min_turns
   const maxTurns = moveMeta.max_turns === '' ? null : moveMeta.max_turns
@@ -20,6 +26,14 @@ module.exports = (db, m) => {
     min_turns: minTurns,
     max_turns: maxTurns,
     min_hits: minHits,
-    max_hits: maxHits
+    max_hits: maxHits,
+    ailment: {
+      url: `http://pokeapi.co/api/v2/move-ailment/${moveMetaAilment.id}/`,
+      name: moveMetaAilment.identifier
+    },
+    category: {
+      url: `http://pokeapi.co/api/v2/move-category/${moveMetaCategory.id}/`,
+      name: moveMetaCategory.identifier
+    }
   }
 }
